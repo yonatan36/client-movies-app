d2a19fc380922f4928d775fb257fae0db823cb74
yonatan taub <yonatantaub36@gmail.com>
2023-07-04 21:51:52 +0300

    about

--- src/components/FotterComp.jsx
+++ src/components/FotterComp.jsx
@@ -1,54 +1,44 @@
-import React from "react";
-import { makeStyles } from "@mui/styles";
-import { Container, Typography, IconButton } from "@mui/material";
+import { Typography, IconButton, Box } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-
-const useStyles = makeStyles((theme) => ({
-  footer: {
-    backgroundColor: "black",
-    color: "white",
-    padding: theme.spacing(1),
-    marginTop: "auto",
-  },
-  icon: {
-    color: "white",
-    marginRight: theme.spacing(1),
-  },
-}));
+import Container from "@mui/material/Container";
 
 const Footer = () => {
-  const classes = useStyles();
-
   return (
-    <footer className={classes.footer}>
-      <Container maxWidth="md">
+    <footer
+      sx={{
+        color: "white",
+        padding: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Container maxWidth="lg">
         <Typography
           variant="body2"
-          align="center"
-          fontWeight={600}
-          color="white"
+          sx={{
+            fontWeight: 600,
+            textAlign: "center",
+          }}
         >
           © {new Date().getFullYear()} yonatan taub
         </Typography>
-        <Typography variant="body2" align="center">
-          <IconButton className={classes.icon} href="https://www.linkedin.com/">
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}>
+          <IconButton sx={{ marginRight: 1 }} href="https://www.linkedin.com/">
             <LinkedInIcon />
           </IconButton>
           <IconButton
-            className={classes.icon}
+            sx={{ marginRight: 1 }}
             href="https://github.com/yonatan36"
           >
             <GitHubIcon />
           </IconButton>
-          <IconButton
-            className={classes.icon}
-            href="https://wa.me/972585668625"
-          >
+          <IconButton href="https://wa.me/972585668625">
             <WhatsAppIcon />
           </IconButton>
-        </Typography>
+        </Box>
       </Container>
     </footer>
   );
