aa4bb7ad4ba74e455850936cad94f5ee29c2878c
yonatan taub <yonatantaub36@gmail.com>
2023-07-23 19:35:09 +0300

    protectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ROUTES from "../../routes/ROUTES";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const isLoggedIn = useSelector((bigState) => bigState.authSlice.isLoggedIn);
-  if (!isLoggedIn) {
-    return <Navigate to={ROUTES.ABOUT} />;
-  } else {
+
+  useEffect(() => {
+    const checkAuthenticationAsync = async () => {
+      try {
+        // After the async check is complete, set the loading state to false
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false); // Ensure loading state is set to false even in case of errors
+      }
+    };
+
+    // Call the async function to check authentication
+    checkAuthenticationAsync();
+  }, [isLoggedIn]);
+
+  if (isLoading) {
+    // Show a loading indicator or a fallback component while checking authentication
+    return <div>Loading...</div>;
+  } else if (isLoggedIn) {
+    // If authenticated, render the protected component
     return element;
+  } else {
+    // If not authenticated, redirect to the "About" page
+    return <Navigate to={ROUTES.ABOUT} />;
   }
 };
 
