5f322cfa1c421deba8a4aecdbc3a5a3abf7ec234
yonatan taub <yonatantaub36@gmail.com>
2023-07-27 12:24:04 +0300

    index

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import store from "./store/bigPie";
 
-axios.defaults.baseURL = process.env.REACT_APP_PROXY + "/api";
+axios.defaults.baseURL = "/api";
 axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
